96b426b134cb4496aef932a56dbaad205f768a46
OMOTO Kenji <doloopwhile@gmail.com>
2015-01-12 20:13:30 +0900

    tmp

--- web/Inbox.elm
+++ web/Inbox.elm
@@ -1,12 +1,14 @@
 module Inbox where
 
 import Date
-import Time
+import Time (..)
 import Signal
 import Graphics.Element (..)
 import Graphics.Input.Field as Field
+import Graphics.Input (button)
 import Text
 import List
+import Json.Encode
 
 type alias Model =
   { tasks      : List Task
@@ -14,23 +16,69 @@ type alias Model =
 
 type alias Task =
   { text: String
+  , createdAt: Time
   }
 
 model : Signal Model
-model = Signal.constant {
+model =
+  -- Signal.constant <|
+  --   case getStorage of
+  --     Nothing -> {tasks = []}
+  --     Just m -> m
+  --
+  Signal.constant {
       tasks = [
         { text = "foo"
-        -- , createdAt = Date.fromTime <| 1421054358 * Time.second
+        , createdAt = 1421054358 * second
+        }
+      , { text = "bar"
+        , createdAt = 1421054000 * second
         }
       ]
     }
 
 main : Signal Element
-main = Signal.map view model
+main = Signal.map2 view model (Signal.subscribe taskContent)
 
-view : Model -> Element
-view m =
-  flow down <| List.map (\t -> Text.plainText t.text ) m.tasks
+taskContent : Signal.Channel Field.Content
+taskContent =
+  Signal.channel Field.noContent
+
+taskEnter : Signal.Channel ()
+taskEnter =
+  Signal.channel ()
+
+
+view : Model -> Field.Content-> Element
+view m fc = let
+    listView =
+      flow down <|
+        (List.map (\t ->
+          flow right [
+            (Text.plainText t.text)
+          , (Text.plainText " ")
+          , (Text.plainText <| dateString t)
+          ]
+        ) m.tasks)
+    inputView = flow right [
+      Field.field Field.defaultStyle (Signal.send taskContent) "input your task" fc
+    , button (Signal.send taskEnter ()) "Add"
+    , Text.plainText fc.string
+    ]
+  in
+    flow down [inputView, listView]
+
+dateString : Task -> String
+dateString t =
+  let
+    d = Date.fromTime t.createdAt
+  in
+    (toString <| Date.year d)
+  ++ "/" ++ (toString <| Date.month d)
+  ++ "/" ++ (toString <| Date.day d)
+  ++ " " ++ (toString <| Date.hour d)
+  ++ ":" ++ (toString <| Date.minute d)
+
 
 -- interactions with localStorage to save the model
 port getStorage : Maybe Model
